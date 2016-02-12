0a40e95f8cc0ee72ea08a39466ae3f683de19d4e
Akash Thakur <skyatwork007@gmail.com>
2016-02-12 21:37:15 +0530

    sky

---
+++ scrapper task (incomplete)/task 1.py
@@ -0,0 +1,143 @@
+from urllib.request import urlopen
+from urllib.error import HTTPError
+from bs4 import BeautifulSoup
+
+
+try:
+    html = urlopen("www.differencebetween.com")
+    except HTTPError as e:
+    return None
+
+try:
+    bsObj = BeautifulSoup(html.read())
+    except AttributeError as e:
+    return None
+    
+
+
+
+<body class="single single-post postid-59305 single-format-standard custom-background custom-header header-image content-sidebar"
+itemscope="itemscope" itemtype="http://schema.org/WebPage"><script type="text/javascript">
+"https://connect.facebook.net/en_US/sdk.js&amp;version=v2.0" async="">
+</script><div style="position: absolute; top: -10000px; height: 0px; width: 0px;">
+<div><iframe name="f18d22a41" frameborder="0" allowtransparency="true" allowfullscreen="true" scrolling="no"
+src="https://www.facebook.com/connect/ping?client_id=172525162793917&amp;domain=www.differencebetween.com&amp;origin=1&amp;redirect_uri=http%3A%2F%2Fstaticxx.facebook.com%2Fconnect%2Fxd_arbiter.php%3Fversion%3D42%23cb%3Df2aa9e09d4%26domain%3Dwww.differencebetween.com%26origin%3Dhttp%253A%252F%252Fwww.differencebetween.com%252Ff56f70d9c%26relation%3Dparent&amp;response_type=token%2Csigned_request%2Ccode&amp;sdk=joey" style="display: none;">
+</iframe></div></div><div style="position: absolute; top: -10000px; height: 0px; width: 0px;"><div><iframe name="fb_xdm_frame_http"
+frameborder="0" allowtransparency="true" allowfullscreen="true" scrolling="no" title="Facebook Cross Domain Communication Frame" aria-hidden="true"
+tabindex="-1" id="fb_xdm_frame_http"
+src="http://staticxx.facebook.com/connect/xd_arbiter.php?version=42#channel=f56f70d9c&amp;origin=http%3A%2F%2Fwww.differencebetween.com"
+style="border: none;"></iframe><iframe name="fb_xdm_frame_https" frameborder="0" allowtransparency="true" allowfullscreen="true"
+scrolling="no" title="Facebook Cross Domain Communication Frame" aria-hidden="true" tabindex="-1" id="fb_xdm_frame_https"
+src="https://staticxx.facebook.com/connect/xd_arbiter.php?version=42#channel=f56f70d9c&amp;origin=http%3A%2F%2Fwww.differencebetween.com"
+style="border: none;"></iframe></div></div></div><iframe name="oauth2relay538025556" id="oauth2relay538025556"
+src="https://accounts.google.com/o/oauth2/postmessageRelay?parent=http%3A%2F%2Fwww.differencebetween.com#rpctoken=307285941&amp;forcesecure=1"
+tabindex="-1" style="width: 1px; height: 1px; position: absolute; top: -100px;"></iframe><iframe id="rufous-sandbox" scrolling="no"
+frameborder="0" allowtransparency="true" allowfullscreen="true"
+style="position: absolute; visibility: hidden; display: none; width: 0px; height: 0px; padding: 0px; border: none;"></iframe></body>
+
+
+
+
+<div class="entry-content" itemprop="text">    <script type="text/javascript">
+    // <![CDATA[
+        var disqus_shortname = 'differencebetween';
+        (function () {
+            var nodes = document.getElementsByTagName('span');
+            for (var i = 0, url; i < nodes.length; i++) {
+                if (nodes[i].className.indexOf('dsq-postid') != -1) {
+                    nodes[i].parentNode.setAttribute('data-disqus-identifier', nodes[i].getAttribute('rel'));
+                    url = nodes[i].parentNode.href.split('#', 1);
+                    if (url.length == 1) { url = url[0]; }
+                    else { url = url[1]; }
+                    nodes[i].parentNode.href = url + '#disqus_thread';
+                }
+            }
+            var s = document.createElement('script'); s.async = true;
+            s.type = 'text/javascript';
+            s.src = '//' + disqus_shortname + '.disqus.com/count.js';
+            (document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
+        }());
+    //]]>
+    </script>
+<div align="center" style="float: left; position: relative; margin-top: 0px; padding: 0 10px;">
+<script async="" src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- Arti Top 300 Oct14 Left -->
+<ins class="adsbygoogle" style="display:inline-block;width:300px;height:250px" data-ad-client="ca-pub-4745434794451545" data-ad-slot="4526780888"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+
+</div><h2 class="txtcolor" style="text-align: left;"><strong>Key Difference –&nbsp;Case Study vs Survey<br>
+</strong> <span id="diffbwCompCheck">&nbsp;</span></h2>
+<p style="text-align: justify;">When conducting research case studies and surveys are two <a title="Difference Between Research Methods and Research Methodology" href="http://www.differencebetween.com/difference-between-research-methods-and-vs-research-methodology/">research methods</a> that are employed by researchers. Although both methods are used to gather information, there is a key difference between a case study and a survey. A case study refers to research in which an individual, group or a particular situation is studied. The duration of the study tends to be relatively long. A survey refers to research where data is gathered from an entire <a title="Difference Between Sample and Population" href="http://www.differencebetween.com/difference-between-sample-and-vs-population/">population</a> or a very large sample in order to comprehend the opinions on a particular matter. The <strong>key difference</strong> between the two methods is that while <strong>case studies produce rich descriptive data, surveys do not</strong>. Instead, the data is collected from surveys are more statistically significant.</p>
+<h2 style="text-align: justify;">What is a Case Study?</h2>
+<p style="text-align: justify;">A case study refers to an in-depth study in which an individual, group or a particular situation is studied. This is used in both in <a title="Difference Between Social Science and Natural Science" href="http://www.differencebetween.com/difference-between-social-science-and-vs-natural-science/">natural and social sciences</a>. In the natural sciences, a case study can be used to validate a theory or even a <a title="Difference Between Hypothesis and Theory" href="http://www.differencebetween.com/difference-between-hypothesis-and-vs-theory/">hypothesis</a>. In the social sciences, case studies are used extensively to study human behavior and comprehend various social aspects. For example, in psychology, case studies are conducted to comprehend the individual behavior. In such an instance, the researcher records the entire history of the individual so that it enables him to identify various patterns of behavior. One of the classic examples for a case study is <a href="http://www.differencebetween.com/difference-between-freud-and-vs-jung/">Sigmund Freud</a>’s study of Anna O.</p>
+<p style="text-align: justify;">When speaking of case studies, it must be highlighted that they usually produce rich descriptive data. However, case studies cannot be used to provide generalizations on an entire population since the sample of a case study is usually limited to a single individual or a few individuals. For a case study, various research techniques such as interviews, direct and participatory observation, and documents can be used.</p>
+<p style="text-align: justify;"><img class="aligncenter  wp-image-59318" src="http://files.differencebetween.com/wp-content/uploads/2016/01/Difference_Between_CaseStudy_Survey_Image1.jpg" alt="Difference Between Case Study and Survey" width="550" height="367"></p>
+<h2 style="text-align: justify;">What is a Survey?</h2>
+<p style="text-align: justify;">A survey refers to research where data is gathered from an entire population or a very large sample in order to comprehend the opinions on a particular matter. In the modern society, surveys are often used in politics and marketing. For example, imagine a situation where an organization wishes to understand the opinions of consumers on their latest product. Naturally the organization would conduct a survey to comprehend the opinions of the consumer.</p>
+<p style="text-align: justify;">One of the most powerful research techniques used for surveys is the <a title="Difference Between Questionnaire and Survey" href="http://www.differencebetween.com/difference-between-questionnaire-and-vs-survey/">questionnaire</a>. For this, the researcher creates a set of questions on the topic for which he will gather information from the participants. Unlike case studies, the data gathered from surveys are not very descriptive. Instead, they are statistically significant.</p>
+<p style="text-align: justify;"><img class="aligncenter  wp-image-59319" src="http://files.differencebetween.com/wp-content/uploads/2016/01/Difference_Between_CaseStudy_Survey_Image2.jpg" alt="Key Difference - Case Study vs Survey" width="555" height="416"></p>
+<h2>What is the difference between Case Study and Survey?</h2>
+<h3><span style="color: #000080;">Definitions of Case Study and Survey:</span></h3>
+<p><strong>Case Study:</strong> A case study refers to an in-depth study in which an individual, group, or a particular situation is studied.</p>
+<p><strong>Survey:</strong> A survey refers to research where data is gathered from an entire population or a very large sample in order to comprehend the opinions on a particular matter.</p>
+<h4><span style="color: #000080;">Characteristics of Case Study and Survey:</span></h4>
+<h4><span style="color: #0000ff;">Research Type:</span></h4>
+<p><strong>Case Study:</strong> Case studies are used in <a title="Difference Between Qualitative and Quantitative Research" href="http://www.differencebetween.com/difference-between-qualitative-and-vs-quantitative-research/">qualitative research</a>.</p>
+<p><strong>Survey:</strong> Surveys are mostly used in quantitative research.</p>
+<h4><span style="color: #0000ff;">Data:</span></h4>
+<p><strong>Case Study:</strong> Case studies produce rich in depth data.</p>
+<p><strong>Survey:</strong> Surveys produce numerical data.</p>
+<h4><span style="color: #0000ff;">Sample:</span></h4>
+<p><strong>Case Study:</strong> For a case study, a relatively small population is chosen. This can vary from a few individuals to groups.</p>
+<p><strong>Survey:</strong> For a survey, a large population can be used as the sample.</p>
+<p>&nbsp;</p>
+<p><em>Image Courtesy:</em></p>
+<p><em>1.&nbsp;“<a href="Microscopy_lab.jpg">Microscopy lab</a>” by Idaho National Laboratory – Flickr: Microscopy lab. [CC BY 2.0] via Commons</em></p>
+<p><em>2. <a href="https://commons.wikimedia.org/wiki/File%3ASurvey_Research_Books.jpg">Survey Research Books</a>&nbsp;By </em>User:Jtneill<em> (Own work) [Public domain], via Wikimedia Commons</em></p>
+
+<div style=" text-align: center;  margin-bottom: 10px;">
+<script async="" src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- Arti Bott 336 Oct14 -->
+<ins class="adsbygoogle" style="display:inline-block;width:336px;height:280px" data-ad-client="ca-pub-4745434794451545" data-ad-slot="1573314483"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+</div><div class="yarpp-related">
+<p>Related posts:</p><ol>
+<li><a href="http://www.differencebetween.com/difference-between-questionnaire-and-vs-survey/" rel="bookmark" title="Difference Between Questionnaire and Survey">Difference Between Questionnaire and Survey </a></li>
+<li><a href="http://www.differencebetween.com/difference-between-case-study-and-experiment/" rel="bookmark" title="Difference Between Case Study and Experiment">Difference Between Case Study and Experiment </a></li>
+<li><a href="http://www.differencebetween.com/difference-between-case-study-and-vs-case-history/" rel="bookmark" title="Difference Between Case Study and Case History">Difference Between Case Study and Case History </a></li>
+<li><a href="http://www.differencebetween.com/difference-between-case-study-and-vs-phenomenology/" rel="bookmark" title="Difference Between Case Study and Phenomenology">Difference Between Case Study and Phenomenology </a></li>
+<li><a href="http://www.differencebetween.com/difference-between-case-study-and-vs-descriptive-approach-to-research/" rel="bookmark" title="Difference Between Case Study and Descriptive Approach to Research">Difference Between Case Study and Descriptive Approach to Research </a></li>
+</ol>
+</div>
+</div>
+
+
+
+
+
+
+
+
+
+
+# item1  and 2  for the table
+
+<strong>Key Difference –&nbsp;Case Study vs Survey<br>
+</strong>
+
+
+
+
+
+div class="entry-content"  itemprop="text"
+
+all the p tags inside this  are content  for item 3
+
+
+
+
+
